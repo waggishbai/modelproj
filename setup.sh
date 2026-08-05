c254d8b83d73d860d4bd6727c2b5fb632af5b583
waggishbai <owenb9479@gmail.com>
2026-08-04 20:44:53 -0700

    setup.sh fixed hopefully

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@ git clone https://github.com/glfw/glfw
 git clone https://github.com/g-truc/glm
 mkdir objloader
 cd objloader
-git clone https://github.com/tinyobjloader/tinyobjloader/tiny_obj_loader.h
+git clone https://github.com/tinyobjloader/tinyobjloader
+cp tinyobjloader/tiny_obj_loader.h ./
+rm -r tinyobjloader/*
+rmdir tinyobjloader
